48c840961e0d62c4fcb0e8dd3ebf0d1ba3b2f0eb
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-11-29 18:49:04 -0800

    This is the verison that works.

--- doit.sh
+++ doit.sh
@@ -2,3 +2,8 @@
 set -x
 
 fontforge -script irish_ligatures.py Gentium-Regular.ttf Gentium-Buailte-Regular.ttf 
+
+#fontforge -script irish_ligatures.py EBGaramond-VariableFont_wght.ttf EBGaramond-VariableFont-Buailte_wght.ttf
+
+#fontforge -script irish_ligatures.py SourceSans3-VariableFont_wght.ttf  SourceSans3-VariableFont-Buailte_wght.ttf 
+
